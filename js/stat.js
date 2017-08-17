07293579144802f0c0b15af5e04b71894d648a53
Anastasiya Liapko <anastasiya.liapko@gmail.com>
2017-08-17 18:08:34 +0300

    второе задание с исправлениями

--- js/stat.js
+++ js/stat.js
@@ -1,3 +1,4 @@
+'use strict';
 window.renderStatistics = function (ctx, names, times) {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
@@ -11,32 +12,40 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.fillText('Список результатов:', 120, 60);
 
   var max = -1;
-  var maxIndex = -1;
 
-  for (var i = 0; i < times.length; i++) {
-    var time = times[i];
-    if (time > max) {
-      max = time;
-      maxIndex = i;
+  var getMaxTime = function (i) {
+    for (i = 0; i < times.length; i++) {
+      var time = times[i];
+      if (time > max) {
+        max = time;
+      }
     }
-  }
+  };
+  getMaxTime();
 
   var histogramHeight = 150;
-  var step = histogramHeight / (max - 0);
+  var step = histogramHeight / -max;
   var barWidth = 40;
   var indent = 50;
   var initialX = 150;
-  var initialY = 100;
+  var initialY = 250;
   var pos = names.indexOf('Вы');
+  var i;
 
   ctx.textBaseline = 'top'; // Рисуем надпись от левого верхнего угла
-  for(var i = 0; i < times.length; i++) {
-    ctx.fillStyle = 'rgba(0, 0, 255, 1)';
-    ctx.fillRect(initialX + (barWidth + indent) * i, initialY, barWidth, times[i] * step);
-    ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    ctx.fillRect(initialX + (barWidth + indent) * i, initialY, barWidth, pos * step);
-    ctx.fillStyle = '#000';
-    ctx.fillText(names[i], initialX + (barWidth + indent) * i, histogramHeight + 100);
-    ctx.fillText(~~times[i], 150 + 90 * i, histogramHeight - 70);
-  }
+
+  var getBarHeight = function () {
+    for (i = 0; i < times.length; i++) {
+      if (pos === i) {
+        ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+      } else {
+        ctx.fillStyle = 'rgba(0, 0, 255, 1)';
+      }
+      ctx.fillRect(initialX + (barWidth + indent) * i, initialY, barWidth, times[i] * step);
+      ctx.fillStyle = '#000';
+      ctx.fillText(names[i], initialX + (barWidth + indent) * i, histogramHeight + 100);
+      ctx.fillText(Math.round(times[i]), 150 + 90 * i, histogramHeight - 70);
+    }
+  };
+  getBarHeight();
 };
