205cb3355b128ef94450af66b804bcc3b8d5383d
Anastasiya Liapko <anastasiya.liapko@gmail.com>
2017-08-18 22:32:54 +0300

    второе задание: разделение функций

--- js/stat.js
+++ js/stat.js
@@ -1,5 +1,5 @@
 'use strict';
-window.renderStatistics = function (ctx, names, times) {
+window.renderStatistics = function (ctx) {
   ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
   ctx.fillRect(110, 20, 420, 270);
   ctx.fillStyle = 'white';
@@ -10,42 +10,44 @@ window.renderStatistics = function (ctx, names, times) {
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', 120, 40);
   ctx.fillText('Список результатов:', 120, 60);
-
-  var max = -1;
-
-  var getMaxTime = function (i) {
-    for (i = 0; i < times.length; i++) {
-      var time = times[i];
-      if (time > max) {
-        max = time;
-      }
-    }
-  };
-  getMaxTime();
-
-  var histogramHeight = 150;
-  var step = histogramHeight / -max;
-  var barWidth = 40;
-  var indent = 50;
-  var initialX = 150;
-  var initialY = 250;
-  var pos = names.indexOf('Вы');
-  var i;
-
-  ctx.textBaseline = 'top'; // Рисуем надпись от левого верхнего угла
-
-  var getBarHeight = function () {
-    for (i = 0; i < times.length; i++) {
-      if (pos === i) {
-        ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      } else {
-        ctx.fillStyle = 'rgba(0, 0, 255, 1)';
-      }
-      ctx.fillRect(initialX + (barWidth + indent) * i, initialY, barWidth, times[i] * step);
-      ctx.fillStyle = '#000';
-      ctx.fillText(names[i], initialX + (barWidth + indent) * i, histogramHeight + 100);
-      ctx.fillText(Math.round(times[i]), 150 + 90 * i, histogramHeight - 70);
-    }
-  };
-  getBarHeight();
 };
+window.renderStatistics();
+
+var max = -1;
+
+var getMaxTime = function (times) {
+  for (i = 0; i < times.length; i++) {
+    var time = times[i];
+    if (time > max) {
+      max = time;
+    }
+  }
+};
+getMaxTime();
+
+var histogramHeight = 150;
+var step = histogramHeight / -max;
+var barWidth = 40;
+var indent = 50;
+var initialX = 150;
+var initialY = 250;
+var i;
+
+var getBarHeight = function (ctx, names, times) {
+  var pos = names.indexOf('Вы');
+  for (i = 0; i < times.length; i++) {
+    if (pos === i) {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = 'rgb(0, 0, 255)';
+      ctx.globalAlpha = Math.random();
+    }
+    ctx.fillRect(initialX + (barWidth + indent) * i, initialY, barWidth, times[i] * step);
+    ctx.fillStyle = '#000';
+    ctx.globalAlpha = 1;
+    ctx.textBaseline = 'top'; // Рисуем надпись от левого верхнего угла
+    ctx.fillText(names[i], initialX + (barWidth + indent) * i, histogramHeight + 100);
+    ctx.fillText(Math.round(times[i]), initialX + (barWidth + indent) * i, times[i] * step + 230);
+  }
+};
+getBarHeight();
