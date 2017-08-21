e80d16e1c068c583c70da5c313c2ded81fa65355
Anastasiya Liapko <anastasiya.liapko@gmail.com>
2017-08-21 17:25:10 +0300

    разделение функций

--- js/stat.js
+++ js/stat.js
@@ -1,39 +1,24 @@
 'use strict';
-window.renderStatistics = function (ctx) {
-  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
-  ctx.fillRect(110, 20, 420, 270);
-  ctx.fillStyle = 'white';
-  ctx.strokeRect(100, 10, 420, 270);
-  ctx.fillRect(100, 10, 420, 270);
-
-  ctx.fillStyle = '#000';
-  ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', 120, 40);
-  ctx.fillText('Список результатов:', 120, 60);
-};
-window.renderStatistics();
-
-var max = -1;
 
 var getMaxTime = function (times) {
-  for (i = 0; i < times.length; i++) {
+  var max = -1;
+  for (var i = 0; i < times.length; i++) {
     var time = times[i];
     if (time > max) {
       max = time;
     }
   }
+  return max;
 };
-getMaxTime();
 
-var histogramHeight = 150;
-var step = histogramHeight / -max;
-var barWidth = 40;
-var indent = 50;
-var initialX = 150;
-var initialY = 250;
-var i;
-
-var getBarHeight = function (ctx, names, times) {
+var getHistogram = function (ctx, names, times, max) {
+  var histogramHeight = 150;
+  var step = histogramHeight / -max;
+  var barWidth = 40;
+  var indent = 50;
+  var initialX = 150;
+  var initialY = 250;
+  var i;
   var pos = names.indexOf('Вы');
   for (i = 0; i < times.length; i++) {
     if (pos === i) {
@@ -50,4 +35,18 @@ var getBarHeight = function (ctx, names, times) {
     ctx.fillText(Math.round(times[i]), initialX + (barWidth + indent) * i, times[i] * step + 230);
   }
 };
-getBarHeight();
+
+window.renderStatistics = function (ctx, names, times) {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.7)';
+  ctx.fillRect(110, 20, 420, 270);
+  ctx.fillStyle = 'white';
+  ctx.strokeRect(100, 10, 420, 270);
+  ctx.fillRect(100, 10, 420, 270);
+
+  ctx.fillStyle = '#000';
+  ctx.font = '16px PT Mono';
+  ctx.fillText('Ура вы победили!', 120, 40);
+  ctx.fillText('Список результатов:', 120, 60);
+  var maxTime = getMaxTime(times);
+  getHistogram(ctx, names, times, maxTime);
+};
